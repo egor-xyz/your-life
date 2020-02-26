47186fe35903322ea27f3ff4c9648ebf45ca5d82
egor-xyz <i@egor.xyz>
2020-02-26 14:52:56 +0200

    Updates

---
+++ precache-manifest.8653d685f4cb5f494962f52c19a016bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a69549c9344211a17175ddc4b6e13f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9bb02eab0d858c2ce0d",
+    "url": "/static/css/main.e93471bc.chunk.css"
+  },
+  {
+    "revision": "a93acd53c56e496dd2bd",
+    "url": "/static/js/2.e18ab93d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e18ab93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9bb02eab0d858c2ce0d",
+    "url": "/static/js/main.f2172191.chunk.js"
+  },
+  {
+    "revision": "9c6007059573b7064022",
+    "url": "/static/js/runtime-main.2735a873.js"
+  }
+]);
